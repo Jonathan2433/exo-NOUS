777f2f8d1a578ee15c29992fc0362034b3875334
Jonathan Dupau <jonathan.dupau24@gmail.com>
2022-06-23 15:50:54 +0200

    add home page + add scss to symfony + add somes styles for header and cards

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,16 +10,16 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(HeroRepository $heroRepository, TeamRepository $teamRepository): Response
     {
-        $heroes = $heroRepository->findAll();
-
         $teams = $teamRepository->findAll();
-        $hero = $heroRepository->find(1);
+
+//        $teams = $teamRepository->findAll();
+//        $hero = $heroRepository->find(1);
 //        dd($hero->getTeam());
         return $this->render('home/index.html.twig', [
-            'heroes' => $heroes,
+            'teams' => $teams,
         ]);
     }
 }
